006d8df2303e61d73984c493c856fbd3bec16147
vdrahnieva-crypto <vdrahnieva@gmail.com>
2026-05-12 23:47:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -12,16 +13,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Привет! Бот работает 🚀"
-    )
+    await update.message.reply_text("Привет! Бот работает 🚀")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
 
-def main():
+async def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
@@ -29,8 +28,8 @@ def main():
 
     print("Бот запущен...")
 
-    app.run_polling()
+    await app.run_polling()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
